b28bcdbf47f7e4926ce44e4b011aab01723b5bde
Brennan Boman <brennanboman@Brennans-Air.hsd1.ut.comcast.net>
2017-01-27 19:35:50 -0700

    mostly finished

--- quizApp.js
+++ quizApp.js
@@ -1,31 +1,43 @@
-//We're going to create the JS for a basic quiz application. 
+//We're going to create the JS for a basic quiz application.
 
 //Let's think about the nature of this quiz app first. We're going to be creating lots of user objects, and we're
-//also going to be creating lots of Question objects. Those would make two perfectly good constructors. 
+//also going to be creating lots of Question objects. Those would make two perfectly good constructors.
 
 //Create a QuizUser constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
-  //code here
+function QuizUser(name, email, password, totalScore){
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.totalScore = totalScore;
+}  //code here
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
-  //code here
+  function Question(title, answersArray, rightAnswer, difficulty) {
+    this.title = title;
+    this.answersArray = answersArray;
+    this.rightAnswer = rightAnswer
+    this.difficulty = difficulty;
+  }//code here
 
 
 //Create a quizUsers Array which is going to hold all of our users.
 
-  //code here
+  var quizUsers = []//code here
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
-  //code here
+  var brett = new QuizUser('Brett', 'brett@gmail.com', '123', '80')
+  var dillon = new QuizUser('Dillon', 'dildog@yahoo.com', '456', '85')
+  var colt = new QuizUser('Colton', 'coltino@ymail.com', '789', '90')//code here
 
-
+quizUsers.push(brett, dillon, colt)
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
+  var questions = []//code here
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -34,15 +46,22 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+var question1 = ['T/F: Inheritance is achieved in JavaScript through Prototypes?']
+var question2 = ['T/F: JavaScript is just a scripting version of Java']
+var question3 = ["T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"]  //code here
 
 
 //Now push all of your instances of Question into the questions Array
 
-  //code here
+  questions.push(question1, question2, question3)//code here
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
-  //code here
+  for (var i = 0; i < questions.length; i++){
+    console.log(questions[i])
+  }
 
+  for (var i = 0; i < quizUsers.length; i++){
+    console.log(quizUsers[i])
+  }//code here
